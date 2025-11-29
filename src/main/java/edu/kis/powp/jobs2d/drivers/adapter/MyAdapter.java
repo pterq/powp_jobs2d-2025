a8d57c933c18a6db8fe6afd3fe6ee82aedbb9ed4
Piotr Owczarek <piotr2785.owca@gmail.com>
2025-11-29 11:47:52 +0100

    poprawne wyświetlanie figury "Figure Joe 1"

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/MyAdapter.java
@@ -26,6 +26,7 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
+		setPosition(x, y);
 
 		drawLine(line);
 	}
